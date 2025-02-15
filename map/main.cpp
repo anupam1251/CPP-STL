ebd7255789f72eab4457be8f900aed06e58833ea
Anupam Chaudhary <ak47.anupam125@gmail.com>
2025-02-16 00:50:30 +0530

    added map stl functionality

--- map/main.cpp
+++ map/main.cpp
@@ -1,3 +1,56 @@
+#include <iostream>
+#include <vector>
+#include <map>
+#include <unordered_map>
+
 int main() {
 
+	std::vector<int> arr{ 1, 3, 3, 2, 3, 4, 1, 3 }; // uniform initialization c++11
+
+	std::map<int, int> freqMap;
+	std::unordered_map<int, int> freqhashMap;
+
+	for (const auto& val : arr) {
+		freqMap[val]++;
+	}
+
+	for (const auto& val : arr) {
+		freqhashMap[val]++;
+	}
+
+	freqMap.insert({ 5, 0 });
+	freqMap.emplace( 6, 0 );
+
+	for (const auto& [key, value] : freqMap) {
+		std::cout << key << " " << value << '\n';
+	}
+	std::cout << std::endl;
+
+
+	auto it = freqMap.find(3);
+	if (it == freqMap.end()) {
+		std::cout << "3 is not in map" << '\n';
+	}
+	else {
+		std::cout << "frequncy of 3 is : " << it->second << '\n' << std::endl;;
+	}
+
+	for (auto it = freqMap.begin(); it != freqMap.end(); ++it) {
+		std::cout << it->first << " " << it->second << '\n';
+	}
+	std::cout << std::endl;
+
+	for (const auto& pair : freqMap) {
+		std::cout << pair.first <<" " << pair.second << '\n';
+	}
+	std::cout << std::endl;
+
+	//structure binding c++17
+	for (const auto& [key, value] : freqhashMap) {
+		std::cout << key << " " << value << '\n';
+	}
+
+	
+
+	return 0;
 }
